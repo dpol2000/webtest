b0ee95e9533477f4c58371c7b85dfdb575c489bd
Dmitry <dpol2000@gmail.com>
2016-08-08 18:01:09 +0400

    tests

--- etest/tests.py
+++ etest/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from utils import normalize
 
-# Create your tests here.
+class UtilsTest(TestCase):
+
+    def test_normalize(self):
+        self.assertEqual(normalize('asd'), 'ASD')
+        self.assertEqual(normalize('aSd'), 'ASD')
+        self.assertEqual(normalize('ASd'), 'ASD')
+        self.assertEqual(normalize('ASD'), 'ASD')
+        self.assertEqual(normalize('aSD'), 'ASD')
+        self.assertEqual(normalize('asd.'), 'ASD')
+        self.assertEqual(normalize('ASD.'), 'ASD')
+        self.assertEqual(normalize('AS.D'), 'AS.D')
+        self.assertEqual(normalize('as.d'), 'AS.D')
+
+
+class ViewsTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
